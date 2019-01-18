6f2c727989b62869fc556bafea94d4cfff34234a
Olivia Osborn <olioz95@gmail.com>
2019-01-18 12:25:38 -0500

    got className to change when tab is clicked

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -65,8 +65,8 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} selectedTab={this.selected} selectTabHandler={this.changeSelected} />
-        <Cards cards={this.filterCards()} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
+        <Cards cards={this.filterCards} />
       </div>
     );
   }
