909a4d99aa7d896e2e30263594d4dc50401f2687
Olivia Osborn <olioz95@gmail.com>
2019-01-18 13:52:26 -0500

    fixed some styling and added indexes to map functions

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,47 +1,19 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from "prop-types"
-import styled from "styled-components";
-
-const StyledTabs = styled.div`
-border-bottom: 1px solid lightgrey;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: row;
-width: 100%;
-height: 47px;
-background-color: #fff;
-
-@media (min-width: 1280px) {
-  width: 1280px;
-}
-`
-
-const Topcs = styled.div`
-display: flex;
-justify-content: none;
-align-items: center;
-flex-direction: row;
-`
-const Title = styled.span`
-font-size: 12px;
-letter-spacing: 1px;
-font-weight: bold;
-`
 
 const Tabs = props => {
   return (
-    <StyledTabs>
-      <Topcs>
-        <Title >TRENDING TOPICS:</Title>
+    <div className="tabs">
+      <div className="topics">
+        <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-        {props.tabs.map(tab => {
-          return <Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler}/>
+        {props.tabs.map((tab, index) => {
+          return <Tab key={index} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler}/>
         })}
-      </Topcs>
-    </StyledTabs>
+      </div>
+    </div>
   );
 };
 
